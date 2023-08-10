e04421636c14548b101042381e69931ff5e2d71f
SonishShrestha <sanish.shrestha5401@gmail.com>
2023-08-10 20:01:17 +0545

    done

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,57 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class MainPage extends StatelessWidget {
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:project_using_provider/model/dummy_json_model.dart';
+
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<Products> listOfProducts = [];
+
+  List categorys = [];
+
+  // Future<List<Products>> getProducts() async {
+  //   Dio dio = Dio();
+  //   final response = await dio.get('https://dummyjson.com/products');
+  //   final datas = DummyJson.fromJson(response.data);
+
+  //   listOfProducts = datas.products;
+  // }
+
+  getByCategory() async {
+    Dio dio = Dio();
+
+    final dataByCategory =
+        await dio.get("https://dummyjson.com/products/categories");
+    categorys = dataByCategory.data;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // getProducts();
+    getByCategory();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Project Using Provider'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: categorys.map((e) {
+              return Text(e);
+            }).toList(),
+          ),
+        ));
   }
 }
