7ed8522443e48d6c02ec80a7e2f5762c0de785e9
SonishShrestha <sanish.shrestha5401@gmail.com>
2023-08-18 20:33:51 +0545

    edded button

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -43,6 +43,16 @@ class _MainPageState extends State<MainPage> {
     });
   }
 
+  Future<DummyJson> getByCategoryName(String category) async {
+    Dio dio = Dio();
+
+    final responseDataByCategoryName =
+        await dio.get('https://dummyjson.com/products/category/${category}');
+    final getDataByCategoryName =
+        DummyJson.fromJson(responseDataByCategoryName.data);
+    return getDataByCategoryName;
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -58,12 +68,81 @@ class _MainPageState extends State<MainPage> {
         appBar: AppBar(
           title: Text('Project Using Provider'),
         ),
-        body: Column(
-          children: categories.map((e) {
-            return Column(
-              children: [Text(e.toString())],
-            );
-          }).toList(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                children: categories.map((e) {
+                  return Column(
+                    children: [
+                      Text(e.toString()),
+                      FutureBuilder(
+                        future: getByCategoryName(e.toString()),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                children: snapshot.data!.products.map((e) {
+                                  return Card(
+                                      margin: EdgeInsets.all(20),
+                                      elevation: 10,
+                                      shadowColor: Colors.black,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      color: Colors.grey,
+                                      child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 10, vertical: 20),
+                                          child: Column(children: [
+                                            Image.network(
+                                              e.images[0],
+                                              width: 150,
+                                              height: 150,
+                                            ),
+                                            Text(
+                                              e.title,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20),
+                                            ),
+                                            Text(
+                                              e.brand,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 18),
+                                            ),
+                                            Text(
+                                              e.category,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 15),
+                                            ),
+                                            Text(
+                                              '${e.price}\$',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 15),
+                                            ),
+                                            ElevatedButton(
+                                                onPressed: () {},
+                                                child: Text('Add To Cart'))
+                                          ])));
+                                }).toList(),
+                              ),
+                            );
+                          } else {
+                            return CircularProgressIndicator();
+                          }
+                        },
+                      )
+                    ],
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
         ));
   }
 }
